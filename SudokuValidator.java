19cc4be8596c1c2867c5f354eaeab8a2cf819333
unknown <fabridamato12@gmail.com>
2026-04-09 10:39:51 -0500

    feat: validacion completa sudoku

--- SudokuValidator.java
+++ SudokuValidator.java
@@ -1,7 +1,20 @@
 public class SudokuValidator {
 
     public static void main(String[] args) {
-        int[][] sudoku = {
+
+        int[][] sudokuValido = {
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+
+        int[][] sudokuInvalido = {
                 {5, 3, 4, 6, 7, 8, 9, 1, 2},
                 {6, 7, 2, 1, 9, 5, 3, 4, 8},
                 {1, 9, 8, 3, 4, 2, 5, 6, 7},
@@ -13,6 +26,14 @@ public class SudokuValidator {
                 {3, 4, 5, 2, 8, 6, 1, 7, 5}
         };
 
+        System.out.println("=== Sudoku Válido ===");
+        ejecutarValidacion(sudokuValido);
+
+        System.out.println("\n=== Sudoku Inválido ===");
+        ejecutarValidacion(sudokuInvalido);
+    }
+
+    public static void ejecutarValidacion(int[][] sudoku) {
         imprimirMatriz(sudoku);
 
         boolean filasValidas = validarFilas(sudoku);
